4193a0290d57fd1574e6b38d3af9ab8e73edc110
flowerinthenight <root@flowerinthenight.com>
2024-10-31 16:45:09 +0900

    initial

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:          "thea",
+		Short:        "A simple port-forward wrapper tool for multiple pods/deployments/services",
+		Long:         `A simple port-forward wrapper tool for multiple pods/deployments/services.`,
+		Run:          run,
+		SilenceUsage: true,
+	}
+)
+
+func run(cmd *cobra.Command, args []string) {
+}
 
 func main() {
 	log.Println("hello")
