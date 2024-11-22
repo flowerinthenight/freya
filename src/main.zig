97750fd4c4afa08927c51c7eb2675cf2e6f7d5f1
flowerinthenight <root@flowerinthenight.com>
2024-11-23 04:45:04 +0900

    socket sink for hedged

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,33 @@
 const std = @import("std");
+const log = std.log;
 const print = std.debug.print;
+const Server = std.net.StreamServer;
 const c = @cImport({
     @cInclude("bindings.h");
 });
 
-pub fn main() !void {
+fn ffi() void {
     print("{d}\n", .{c.ffi()});
+}
+
+pub fn sock() !void {
+    const path = "/tmp/hedged-notify.sock";
+    const addr = try std.net.Address.initUnix(path);
+    var server = try addr.listen(.{});
+    defer server.deinit();
+
+    var buff: [500]u8 = undefined;
+    while (true) {
+        log.info("waiting for connections...", .{});
+        const conn = try server.accept();
+        const bytes = try conn.stream.read(&buff);
+        log.info("read: {any}, {s}", .{ bytes, buff });
+    }
+}
+
+pub fn main() !void {
+    if (false) ffi();
+    if (true) try sock();
 }
 
 test "simple test" {
